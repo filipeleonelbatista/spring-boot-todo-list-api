941c90217d883e8911d63ddbeb2c73a587e31c9d
Filipe Batista <filipe.batista@externos-br.cencosud.com>
2023-10-13 09:11:08 -0300

    first commit

---
+++ src/test/java/br/com/filipeleonelbatista/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filipeleonelbatista.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
